cbb3fe4a72d8aed971e0a42f62c1664be368b0b1
Thr1ve <fict1on.buhler@gmail.com>
2016-09-27 19:03:06 -0400

    format as date before storing in db and log all updates to log file

--- bin/updateDb.js
+++ bin/updateDb.js
@@ -1,20 +1,29 @@
+const moment = require('moment');
 const r = require('rethinkdb');
 const connect = require('./connect');
 const getTrafficData = require('./getTrafficData');
+const writeToLog = require('./writeToLog');
 
 const db = string => r.db(string);
 
 const insertItem = (item, conn) =>
   db('nycTraffic').table('data').insert(
     // TODO: create the id when we format the data in getTrafficData.js instead?
-    Object.assign({}, item, { id: [item.linkName, item.DataAsOf] })
+    Object.assign({}, item, {
+      id: [item.linkName, new Date(item.DataAsOf)], 
+      DataAsOf: new Date(item.DataAsOf)
+    })
   ).run(conn)
 
 module.exports = function updateDb() {
   getTrafficData().then(data => {
     connect()
       .then(conn => Promise.all(data.map(item => insertItem(item, conn)))
-        .then(res => console.log(`${res.filter(item => !item.errors).length} items added`))
+        .then(res => {
+          const nItems = res.filter(item => !item.errors).length;
+          console.log(`${nItems} items added`);
+          writeToLog(`${moment(Date.now())}  --  ${nItems} added`)
+        })
         .then(() => conn.close())
       )
       .catch(err => console.log(err));
---
+++ bin/writeToLog.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+module.exports = function writeToLog(str) {
+  return new Promise(function(resolve, reject) {
+    fs.appendFile('./log.txt', str + '\n', (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    })
+  })
+}
