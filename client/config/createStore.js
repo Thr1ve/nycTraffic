887b144d070a39710471fd679dc06c121f22e276
Thr1ve <fict1on.buhler@gmail.com>
2016-09-27 23:41:49 -0400

    update createStore and add redux-thunk

--- client/config/createStore.js
+++ client/config/createStore.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-export default function configureStore() {
-  return createStore(rootReducer);
+export default function configureStore(initialState) {
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      // This line simply enables the redux dev-tools chrome extension for our app
+      process.env.NODE_ENV !== 'production' && window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
+
+  return store;
 }
