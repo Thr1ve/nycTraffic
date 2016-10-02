4bb44285703dcbb7fa342334c94eeb5e53758eaa
Thr1ve <fict1on.buhler@gmail.com>
2016-10-02 16:47:22 -0400

    add initial getStreets action and path state

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,34 @@
 import axios from 'axios';
 
-export const getPaths = () => (dispatch, getState) => {
-  axios.get('api/asCoordinates')
-    .then(res => console.log(res))
+import waterOutline from '../waterOutline.js';
+import { toSVGCoords } from '../lib'
+
+const settings = {
+  h: 0.43,
+  w: 0.57,
+  m: 350,
+  hSkew: 50,
+  vSkew: 20,
+};
+
+export * from './displayed';
+
+export const SET_PATHS = 'SET_PATHS';
+export const setPaths = paths => ({ type: SET_PATHS, paths });
+
+export const getStreets = () => (dispatch, getState) => {
+  axios.get('api/getStreets')
+    .then(res => {
+      const inStaten = res.data
+        // .filter(street => street.Borough === 'Manhattan')
+        .map(street => street.linkPoints
+          .map(c => toSVGCoords(c, settings))
+          .map(c => `${c.x},${c.y}`)
+          .join(' ')
+        );
+      dispatch(setPaths([
+        ...inStaten
+      ]))
+    })
     .catch(err => console.log(err));
 }
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,22 @@
+import { combineReducers } from 'redux';
+
+import displayed from './displayed';
+import { SET_PATHS } from '../actions';
 
 const defaultState = {
-  foo: 'bar'
+  paths: []
 };
 
-export default function (state = defaultState, action) {
+function paths(state = defaultState, action) {
   switch (action.type) {
+    case SET_PATHS:
+      return {
+        ...state,
+        paths: action.paths
+      };
     default:
       return state;
   }
 }
+
+export default combineReducers({ paths, displayed })
