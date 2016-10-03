804b2006eef59eb2928ff8b4a57968975704ddae
Thr1ve <fict1on.buhler@gmail.com>
2016-10-03 01:00:25 -0400

    only try to drop database if we have one in wipeDb

--- bin/wipeDb.js
+++ bin/wipeDb.js
@@ -3,13 +3,18 @@ const connect = require('../server/lib/connect');
 
 const DATABASE = 'nycTraffic';
 
-console.log(`Dropping database "${DATABASE}"`);
+console.log(`Dropping database "${DATABASE}"...`);
 
-connect()
-  .then(conn => {
-    r.dbDrop(DATABASE).run(conn)
-      .then(() => {
-        console.log('Database dropped.');
-        conn.close();
-      });
-  });
+connect().then(conn => {
+  r.dbList().run(conn).then(list => {
+    if (list.includes(DATABASE)) {
+      return r.dbDrop(DATABASE).run(conn)
+        .then(() => {
+          console.log('Database dropped.');
+          conn.close();
+        });
+    }
+    console.log(`Database "${DATABASE}" not found.`);
+    conn.close();
+  })
+});
